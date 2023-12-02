cffa804dc2f9b586336670e1a4533fb892f387fc
TheBenster <105664089+TheBenster@users.noreply.github.com>
2023-12-02 17:52:34 -0500

    create state and props for header

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,52 @@
-// import { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 import MainForm from "./components/Form";
+import Header from "./components/Header";
+// import { name, updateName } from "./components/Header";
 
 function App() {
+  const [name, setName] = useState("John Doe");
+  const [niche, setNiche] = useState("Web Developer");
+  const [email, setEmail] = useState("JoeRogan@gmail.com");
+  const [phone, setPhone] = useState("123-456-7890");
+  const [address, setAddress] = useState("123 Main Street");
+  //   const handleSubmit = (e) => {
+  //     e.preventDefault();
+  //     setName("");
+  //   };
+  const updateName = (e) => {
+    setName(e.target.value);
+  };
+  const updateNiche = (e) => {
+    setNiche(e.target.value);
+  };
+  const updateEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const updatePhone = (e) => {
+    setPhone(e.target.value);
+  };
+  const updateAddress = (e) => {
+    setAddress(e.target.value);
+  };
   return (
-    <>
-      <p>Test</p>
-      <MainForm />
-    </>
+    <div className="App">
+      <MainForm
+        updateName={updateName}
+        updateNiche={updateNiche}
+        updateEmail={updateEmail}
+        updatePhone={updatePhone}
+        updateAddress={updateAddress}
+      />
+      <Header
+        name={name}
+        niche={niche}
+        email={email}
+        phone={phone}
+        address={address}
+      />
+    </div>
   );
 }
 
